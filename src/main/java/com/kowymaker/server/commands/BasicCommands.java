63368093a5f5ac79f63f3099f7e0ce43fec2b557
KokaKiwi <admin@kokabsolu.com>
2011-10-28 18:53:45 +0200

    Modified basics commands.

--- src/main/java/com/kowymaker/server/commands/BasicCommands.java
+++ src/main/java/com/kowymaker/server/commands/BasicCommands.java
@@ -1,6 +1,9 @@
 package com.kowymaker.server.commands;
 
+import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.UpstreamMessageEvent;
@@ -14,6 +17,7 @@ import com.kowymaker.server.interfaces.CommandSender;
 import com.kowymaker.spec.net.CodecResolver;
 import com.kowymaker.spec.net.MessageHandler;
 import com.kowymaker.spec.net.msg.DisconnectMessage;
+import com.kowymaker.spec.utils.SystemUtils;
 
 /**
  * Basics commands for the server: stop, say and stress (others will be
@@ -72,7 +76,7 @@ public class BasicCommands
             empty = true;
             for (final Task task : tasks)
             {
-                if (task != null)
+                if (task != null && empty)
                 {
                     if (!task.isExecuted())
                     {
@@ -103,6 +107,77 @@ public class BasicCommands
         {
             player.sendMessage(command.getJoinedString(0));
         }
+        return true;
+    }
+    
+    @Command(aliases = { "help" }, max = 1, desc = "Display help", usage = "/<command> [command]")
+    public static boolean help(KowyMakerServer main, CommandContext command,
+            CommandSender sender)
+    {
+        if (command.argsLength() == 0)
+        {
+            List<String> commands = new ArrayList<String>();
+            List<String> descs = new ArrayList<String>();
+            int minSize = 0;
+            for (Method method : main.getCommandsManager().getAliases()
+                    .values())
+            {
+                Command cmd = method.getAnnotation(Command.class);
+                StringBuffer sb = new StringBuffer();
+                for (int i = 0; i < cmd.aliases().length; i++)
+                {
+                    sb.append(cmd.aliases()[i]);
+                    if (i < cmd.aliases().length - 1)
+                    {
+                        sb.append(',');
+                    }
+                }
+                String desc = cmd.usage().replace("<command>",
+                        "[" + sb.toString() + "]");
+                commands.add(desc);
+                descs.add(cmd.desc());
+                if (desc.length() > minSize)
+                {
+                    minSize = desc.length();
+                }
+            }
+            
+            StringBuffer sb = new StringBuffer();
+            sb.append("/================ HELP ===================/");
+            sb.append(SystemUtils.getSystemOS().getLineSeparator());
+            for (int i = 0; i < commands.size(); i++)
+            {
+                sb.append(commands.get(i));
+                for (int j = 0; j < minSize - commands.get(i).length(); j++)
+                {
+                    sb.append(' ');
+                }
+                sb.append("  :  ");
+                sb.append(descs.get(i));
+                if(i < commands.size() - 1)
+                {
+                    sb.append(SystemUtils.getSystemOS().getLineSeparator());
+                }
+            }
+            sender.sendMessage(sb.toString());
+        }
+        else
+        {
+            String c = command.getString(0);
+            Method method = main.getCommandsManager().getAliases().get(c);
+            if(method != null)
+            {
+                Command cmd = method.getAnnotation(Command.class);
+                sender.sendMessage("Usage: "
+                        + cmd.usage().replace("<command>", c));
+            }
+            else
+            {
+                sender.sendMessage("Command didn't exists!");
+            }
+            
+        }
+        
         return true;
     }
 }
